1b86872e99daee5055ec698e056a1648cd376bdb
Nikola165 <nikola123mircic@gmail.com>
2021-04-14 13:46:45 +0200

    Updated GeometryObject.java

--- src/app/mathhelper/shape/GeometryObject.java
+++ src/app/mathhelper/shape/GeometryObject.java
@@ -18,40 +18,4 @@ public abstract class GeometryObject {
 	public void handleInfo(InfoHandler handler) {
 		handler.handle(this.info);
 	}
-	/*protected abstract void calculateArea();
-	protected abstract void calculateScope();*/
-	
-	/*public double getArea() {
-		calculateArea();
-		return this.area;
-	}
-	
-	public double getScope() {
-		calculateScope();
-		return this.scope;
-	}
-	
-	
-	
-	protected void addVertex(int idx,String[] values) {
-		idx--;
-		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		String name = ""+alphabet.charAt(idx%26);
-		for(int i=0;i<idx/26;++i) name+="\'";
-		
-		this.v.add(new Vertex3D(name,Double.parseDouble(values[1]),Double.parseDouble(values[2]),Double.parseDouble(values[3])));
-		this.center = getCenterCords();
-	}
-	
-	public List<Vertex3D> getVertices(){
-		return this.v;
-	}
-	
-	public List<Edge> getEdges() {
-		return this.e;
-	}
-
-	public void setCenter(Vertex3D center) {
-		this.center = center;
-	}*/
 }
