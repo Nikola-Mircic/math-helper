992a019e42e7034f5fc1e9289a0b33b9e0203502
Nikola165 <nikola123mircic@gmail.com>
2021-08-20 22:55:49 +0200

    Pulled updates of other branches

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -p "./lib/javafx-sdk-16/lib" --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.web,javafx.swing --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar demo.jar
